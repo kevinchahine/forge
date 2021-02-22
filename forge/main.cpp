e679c02ebf9fea00475d21df2b1a1fad54d7ab48
Kevin Chahine <kchahine17@gmail.com>
2021-02-21 23:51:47 -0500

    Added main.cpp

---
+++ forge/main.cpp
@@ -0,0 +1,20 @@
+/*
+------- FORGE ---------
+*** Forged by Fire ***
+
+*/
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+
+
+	//this_thread::sleep_for(chrono::seconds(2));
+	cin.get();
+	return 0;
+}
