15c44979f66343798283fc7e18accbcb78dd23c5
Kevin Chahine <kchahine17@gmail.com>
2021-03-06 00:23:27 -0500

    Added Node class

--- forge/Node.cpp
+++ forge/Node.cpp
@@ -4,5 +4,37 @@ using namespace std;
 
 namespace forge
 {
+	void Node::expand()
+	{
+		MoveList moves;
 
+		// 1.) --- Generate legal moves ---
+		if (m_position.moveCounter().isWhitesTurn()) {
+			moves = m_position.board().generatePsuedoLegalMovesWhite();	// TODO: these should generate positions
+		}
+		else {
+			moves = m_position.board().generatePsuedoLegalMovesBlack();	// TODO: these should generate positions
+		}
+
+		// 2.) --- Create children nodes ---
+		m_childrenPtrs.clear();
+		m_childrenPtrs.reserve(moves.size());
+
+		for (const auto & move : moves) {
+			unique_ptr<Node> childPtr = make_unique<Node>();
+
+			//m_childrenPtrs.back()->m_position.applyMove(move.second);	// TODO:
+			childPtr->m_parentPtr = this;
+
+			m_childrenPtrs.push_back(std::move(childPtr));
+		}
+
+		// 3.) --- Fix sibling pointers ---
+		for (size_t i = 0; i < m_childrenPtrs.size() - 1; i++) {
+			unique_ptr<Node> & childPtr = m_childrenPtrs.at(i);
+			unique_ptr<Node> & nextChildPtr = m_childrenPtrs.at(i + 1);
+
+			childPtr->m_siblingPtr = nextChildPtr.get();
+		}
+	}
 } // namespace forge
--- forge/Node.h
+++ forge/Node.h
@@ -4,29 +4,53 @@
 
 #include <vector>
 #include <memory>
+#include <mutex>
 
 namespace forge
 {
+	// Part of a NodeTree.
+	// Used in search algorithms to store a Position and branches to other Positions
+	// that are to be searched.
+	// Can also be used to calculate 50 move rule by traversing from a node to its parents.
+	// Multi-threading:
+	//	Some search algorithms like breadth-first search and depth-first search use queues and
+	//	stacks to store their frontiers. A Node Tree can be used instead to allow for more efficient 
+	//	multi-threading. This is because threads can be assigned to search different 
+	//	branches and will need little inter-thread interactions. Each thread can search a Node
+	//	and all its children without having to communicate with other threads.
+	//	Search algorithms that use a queue or stack
+	//	will need a mutex that is constantly locked and unlocked as threads access it greately
+	//	increasing overhead as more and more threads are used.
 	class Node
 	{
 	public:
+		// Takes the board of this Node and generates children nodes using a valid move generator
+		// The Children nodes can be accessed using the .children() method.
+		// If called on a Node with existing children, the original children will be deleted
+		// and replace by the new ones.
+		void expand();
 
+		std::vector<std::unique_ptr<Node>> & children() { return m_childrenPtrs; }
+		const std::vector<std::unique_ptr<Node>> & children() const { return m_childrenPtrs; }
 
 	private:
+		// Keep this here for later.
+		std::mutex m_lock;
+
 		// Stores a position of the game
-		Position position;
+		Position m_position;
 
 		// Address of parent node
 		// if nullptr then this object is the root of the tree
-		// No need to dereference
-		Node * parentPtr = nullptr;
+		// No need to deallocate
+		Node * m_parentPtr = nullptr;
 		
 		// Address of next child node
 		// move efficient than using an tree iterator
-		// No need to dereference
-		Node * siblingPtr = nullptr;
+		// No need to deallocate
+		Node * m_siblingPtr = nullptr;
 
 		// Addresses of children nodes
-		std::vector<std::unique_ptr<Node>> childrenPtrs;
+		std::vector<std::unique_ptr<Node>> m_childrenPtrs;
 	};
 } // namespace forge
