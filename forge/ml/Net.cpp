0e8944048d1b6b06f05d4e40f9c29ba45e398a73
Kevin Chahine <kchahine17@gmail.com>
2022-04-28 04:13:45 +0100

    Trouble moving tensors to GPU

--- forge/ml/Net.cpp
+++ forge/ml/Net.cpp
@@ -2,6 +2,9 @@
 
 #include <torch/csrc/api/include/torch/optim/adam.h>
 #include <torch/csrc/api/include/torch/serialize.h>
+#include <torch/csrc/api/include/torch/cuda.h>
+
+// https://pytorch.org/tutorials/advanced/cpp_frontend.html#writing-the-training-loop	// training in PyTorch (GPU also)
 
 using namespace std;
 
@@ -11,6 +14,18 @@ namespace forge
 	{
 		void Net::train(DataSet& trainingDS, size_t nEpochs)
 		{
+			// Save device which will perform training
+			torch::Device device = torch::kCPU;
+			if (torch::cuda::is_available()) {
+				std::cout << "CUDA is available! Training on GPU." << std::endl;
+				device = torch::kCUDA;
+			}
+
+			cout << "device = " << device << endl;
+
+			// Move network to the training device (will be a no-op if already there)
+			//this->to(device);
+			
 			int counter = 0;
 
 			torch::optim::Adam optimizer(this->parameters(), 0.01);
@@ -19,6 +34,13 @@ namespace forge
 			for (size_t epoch = 0; epoch < nEpochs; epoch++) {
 				TensorPair batch = trainingDS.getNextBatch();
 				
+				// Move training data to training device
+				batch.inputs.to(device);
+				batch.outputs.to(device);
+
+				cout << "inputs exists on: " << batch.inputs.device()
+					<< " index: " << batch.inputs.get_device() << endl;
+
 				// When we reach the end of the dataset, start over until we reach the last epoch
 				if (batch.nSamples() == 0) {
 					trainingDS.reset();
@@ -27,13 +49,17 @@ namespace forge
 				// Reset gradients
 				optimizer.zero_grad();
 
+				cout << "forward" << endl;
 				// Execute the model on the input data
 				torch::Tensor prediction = this->forward(batch.inputs);
 				// Compute a loss value to judge the prediction of our model.
+				cout << "mse_loss" << endl;
 				torch::Tensor loss = torch::mse_loss(prediction, batch.outputs);
+				cout << "backward" << endl;
 				// Compute gradients of the loss w.r.t. the parameters of our model
 				loss.backward();
 
+				cout << "step" << endl;
 				// Update the parameters based on the calculated gradients
 				optimizer.step();
 				
