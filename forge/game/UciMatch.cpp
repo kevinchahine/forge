f57c92897a9813469c6f714cc2991cdc170df0be
Kevin Chahine <kchahine17@gmail.com>
2022-10-29 06:49:04 -0400

    I don't know, its not working

--- forge/game/UciMatch.cpp
+++ forge/game/UciMatch.cpp
@@ -2,6 +2,8 @@
 
 #include <uci/commands/position.h>
 
+#include <thread>	// for hardware_concurrency()
+
 using namespace std;
 
 namespace forge
@@ -13,46 +15,94 @@ namespace forge
 			Position currPos;
 
 			// --- Define Callbacks ---
-			function<void(const uci::Command&)> on_setoption = [&](const uci::Command& cmd)
+			function<void(const uci::commands::setoption&)> on_setoption = [&](const uci::commands::setoption& cmd)
 			{
 				//cout << "We need to setoption: " << cmd << endl;
 			};
 
-			function<void(const uci::Command&)> on_position = [&](const uci::Command& cmd)
+			function<void(const uci::commands::position&)> on_position = [&](const uci::commands::position& cmd)
 			{
-				uci::commands::position pos = cmd;
-				
-				currPos = pos.to_position();
+				currPos = cmd.pos;
 			};
 
-			function<void(const uci::Command&)> on_ucinewgame = [&](const uci::Command& cmd) 
+			function<void(const uci::commands::ucinewgame&)> on_ucinewgame = [&](const uci::commands::ucinewgame& cmd) 
 			{ 
 				currPos.setupNewGame(); 
 			};
 
-			function<void(const uci::Command&)> on_go = [&](const uci::Command& cmd)
+			function<void(const uci::commands::go&)> on_go = [&](const uci::commands::go& cmd)
 			{
 				//uci::go g{ cmd };
 
+				// TODO: A lot goes here
 			};
 
-			m_uciGui.on_setoption = on_setoption;
-			m_uciGui.on_ucinewgame = on_ucinewgame;
-			m_uciGui.on_position = on_position;
+			////m_uciGui.on_setoption = on_setoption;
+			////m_uciGui.on_ucinewgame = on_ucinewgame;
+			////m_uciGui.on_position = on_position;
 
 			// --- Start Communicating ---
 
 			m_uciGui.recv_until_uci();
-			m_uciGui.send_id("Forge", "Kevin \'Micah\' Chahine");
-			
+
+			{
+				uci::commands::id id;
+				id.name = "Forge";
+				m_uciGui.send_id(id);
+
+				id.name.clear();
+				id.author = "Kevin \'Micah\' Chahine";
+				m_uciGui.send_id(id);
+			}
+
 			// .......... Send options here ............
 			
-			m_uciGui.send_uciok();
-			
-			m_uciGui.recv_until_isready();
+			{
+				uci::commands::option op = uci::commands::option::spin{};
+
+				uci::commands::option::spin& spin = op.as<uci::commands::option::spin>();
+				op.option_name = "MultiPV";
+				spin.default_val = std::thread::hardware_concurrency();
+				spin.min = 1;
+				spin.max = std::thread::hardware_concurrency();
+				spin.var = std::thread::hardware_concurrency();
+				
+				cout << "Sending op" << endl;
+				m_uciGui.send_option(op);
+				cout << "done" << endl;
+			}
+
+			{
+				uci::commands::option op = uci::commands::option::string{};
+
+				////op.option_name = "UCI_EngineAbout";
+				////uci::commands::option::string& string_op = op.as<uci::commands::option::string&>(op);
+				////string_op.default = "Hi. I am Forge, an AI powered chess engine written in C++ by Kevin Chahine";
+				////
+				////m_uciGui.send(op);
+			}
+
+			cout << "waiting for isready...";
+			m_uciGui.recv_until_isready();	// setoption commands will be handled as we wait
+			cout << "done" << endl;
+
 			m_uciGui.send_readyok();
+			
+			cout << "waiting for position...";
+			uci::commands::position pos = m_uciGui.recv_until_position();	// maybe we should wait for go instead or even stop?
+			cout << "done" << endl;
 
+			cout << "waiting for go...";
+			uci::commands::go go = m_uciGui.recv_until_go();				// 
+			cout << "done" << endl;
 
+			cout << "waiting for stop...";
+			m_uciGui.recv_until_stop();
+			cout << "got it" << endl;
+
+			cout << "waiting for quit...";
+			m_uciGui.recv_until_quit();
+			cout << "got it" << endl;
 		}
 	} // namespace game
 } // namespace forge
